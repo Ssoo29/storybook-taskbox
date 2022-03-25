378a9513a963e721966f5515863aad4a4e4ea159
김성수 <fereverv@openbase.co.kr>
2022-03-25 17:28:00 +0900

    task red

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,34 +1,16 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div className={`list-item ${state}`}>
-      <label className="checkbox">
-        <input
-          type="checkbox"
-          defaultChecked={state === "TASK_ARCHIVED"}
-          disabled={true}
-          name="checked"
-        />
-        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
-      </label>
-      <div className="title">
-        <input
-          type="text"
-          value={title}
-          readOnly={true}
-          placeholder="Input title"
-        />
-      </div>
-      <div className="actions" onClick={(event) => event.stopPropagation()}>
-        {state !== "TASK_ARCHIVED" && (
-          // eslint-disable-next-line jsx-a11y/anchor-is-valid
-          <a onClick={() => onPinTask(id)}>
-            <span className={`icon-star`} />
-          </a>
-        )}
-      </div>
+    <div className="title">
+      <input
+        type="text"
+        value={title}
+        readOnly={true}
+        placeholder="Input title"
+        style={{ background: "red" }}
+      />
     </div>
   );
 };
